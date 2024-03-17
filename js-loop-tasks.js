e10a169b293c748dd9d94032f6ed6b44606f0738
Fuad-hasan001 <spotlight.neutron0@gmail.com>
2024-03-17 07:49:44 +0600

    js-loop-tasks.js

---
+++ js-loop-tasks.js
@@ -0,0 +1,105 @@
+// tasks-1.1
+/* for(let i=1; i<=200; i++){
+    console.log("Display output: 1 to 200: ", i);
+
+    if(i == 100){
+        break;
+    }
+} */
+
+
+// tasks-1.2: 
+
+/* let sum = 0;
+let num = 1;
+while(num <=100){
+    
+    sum = sum + num;
+
+    if(sum >= 100){
+        break;
+    } 
+    num++;
+}
+console.log(sum); */
+
+
+// Tasks-2.1:
+/* for(let i = 1; i<=40; i++){
+  if(i % 2 !== 0){
+    continue;
+  }
+  else{
+    console.log(i);
+  }
+}
+console.log("\n"); */
+
+
+
+// Tasks-2.2: 
+/* for(let i = 55; i<=85; i++){
+    if(i % 5 == 0){
+        continue;
+    }
+    else{
+        console.log(i);
+    }
+} */
+
+// Tasks-3.1:
+/* for(let i = 1; i<= 60; i++){
+    console.log(i, "I will invest at least 6 hrs every single day for next 60 days!");
+} */
+
+
+
+// Tasks-3.2:
+/* for(let j=61; j<=100; j++){
+    if(j % 2 == 1){
+        console.log(j);
+    }
+} */
+
+
+// Tasks-3.3:
+/* for(let i=78; i <= 98; i++){
+    if(i % 2 == 0){
+        console.log(i);
+    }
+} */
+
+
+// Tasks-3.3:
+/* let sum = 0;
+ for(let i=91; i<= 129; i++){
+    if(i % 2 == 1){
+        sum += i;
+    }
+ }
+ console.log("Sum: ", sum);
+ */
+
+// Tasks-3.4:
+/* for(let i=51; i<= 85; i++){
+    if(i % 2 == 0){
+        sum += i;
+    }
+}
+console.log("Sum: ", sum); */
+
+
+// Tasks-3.5
+/* for(let i = 1; i <=10; i++){
+    console.log(`9 * `+ i + " = ", 9 * i);
+} */
+
+
+// Tasks-3.6: 
+/* for(let i=81; i>=65; i--){
+    console.log(i, count);
+} */
+
+
+// Tasks-3.7 
+// same thing like for-loop 
